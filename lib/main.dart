e9332a9d0e69d60a79926d06cbd74685038db435
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-18 23:30:03 +0530

    created screen and controller folders

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    
     );
   }
 }
