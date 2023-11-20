1ef8b9f36eaaa85a7a78b0d0b49993f282f74040
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-20 23:48:26 +0530

    added some functions

--- lib/controller/global_controller.dart
+++ lib/controller/global_controller.dart
@@ -1,5 +1,27 @@
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:get/get.dart';
 
 class GlobalController extends GetxController {
+  final RxBool isLoading = true.obs;
+  final RxDouble lattittude = 0.0.obs;
+  final RxDouble longittude = 0.0.obs;
+  RxBool checkLoading() => isLoading;
+  RxDouble getLattittude() => lattittude;
+  RxDouble getLongittude() => longittude;
 
-}   
+  void onInit() {
+    if (isLoading.isTrue) {
+      getLocation();
+    }
+    super.onInit();
+  }
+
+  getLocation() async {
+    bool isServiceEnabled;
+    isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isServiceEnabled) {
+      return Future.error("location maariyappo , ellam oke aaayil,");
+    }
+  }
+}
