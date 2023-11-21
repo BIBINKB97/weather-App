b32cc7d6682ea86b551920f72a71cd0af1ed038c
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-21 22:14:05 +0530

    permissions added

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weather_app/controller/global_controller.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final GlobalController globalController =
+        Get.put(GlobalController(), permanent: true);
     return Scaffold(
       appBar: AppBar(),
     );
