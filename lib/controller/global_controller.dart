8a25eefd5b535956564a74c08d671b934ac0e4a0
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-21 21:35:17 +0530

    created controller function

--- lib/controller/global_controller.dart
+++ lib/controller/global_controller.dart
@@ -9,6 +9,7 @@ class GlobalController extends GetxController {
   RxDouble getLattittude() => lattittude;
   RxDouble getLongittude() => longittude;
 
+  @override
   void onInit() {
     if (isLoading.isTrue) {
       getLocation();
@@ -18,10 +19,31 @@ class GlobalController extends GetxController {
 
   getLocation() async {
     bool isServiceEnabled;
+    LocationPermission locationPermission;
+
     isServiceEnabled = await Geolocator.isLocationServiceEnabled();
 
     if (!isServiceEnabled) {
-      return Future.error("location maariyappo , ellam oke aaayil,");
+      return Future.error("location not enabled");
     }
+    // status of Permission
+    locationPermission = await Geolocator.checkPermission();
+
+    if (locationPermission == LocationPermission.denied) {
+      return Future.error("Location permission are denied forever");
+    } else if (locationPermission == LocationPermission.denied) {
+      // request Premission
+      locationPermission = await Geolocator.requestPermission();
+      if (locationPermission == LocationPermission.denied) {
+        return Future.error('Location premission is denied');
+      }
+    }
+    return await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high)
+        .then((value) {
+      lattittude.value = value.latitude;
+      longittude.value = value.longitude;
+      isLoading.value = false;
+    });
   }
 }
