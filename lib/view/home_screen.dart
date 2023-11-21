5861c8d91182598e044490d499bd8eef05295eef
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-21 23:26:18 +0530

    created header

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:weather_app/controller/global_controller.dart';
+import 'package:weather_app/widgets/header.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final GlobalController globalController =
+    final GlobalController globalController   =
         Get.put(GlobalController(), permanent: true);
     return Scaffold(
-      appBar: AppBar(),
+    body: SafeArea(child: 
+    Obx(() => globalController.checkLoading().isTrue ? Center(child: CircularProgressIndicator(),):ListView(
+      scrollDirection: Axis.vertical,
+      children: [
+        Header()
+      ],
+    ))
+    ),
     );
   }
 }
+
