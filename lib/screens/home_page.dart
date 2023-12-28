5fa832b0d4a4c08532e36288fea8430b367ae7e5
bibin k b <bibinkochumalayil007@gmail.com>
2023-12-28 23:30:31 +0530

    home page added

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size=MediaQuery.of(context).size;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation:0,
+      ),
+      body: Container(
+
+
+        height:size.height ,
+        width: size.width,
+
+),
+
+
+
+    );
+  }
+}
